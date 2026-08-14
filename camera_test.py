d5ae460b291f9179ddcad53dd54260481174c75b
Ömer Karagöz <83728691+Fomkar@users.noreply.github.com>
2026-08-14 10:50:58 +0300

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -26,6 +26,7 @@ fps = 0
 while True:
 
     frame = picam2.capture_array()
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
     current_time = time.time()
     fps = 1 / (current_time - prev_time)
